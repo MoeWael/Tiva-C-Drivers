3a3bf6ea1cf007d68c4aff6b9e4120a2945d3c2b
MoeWael <100419696+MoeWael@users.noreply.github.com>
2022-08-11 15:43:34 +0200

    Modified LED App

--- Src/Apps/main.c
+++ Src/Apps/main.c
@@ -2,6 +2,7 @@
 #include "PORT.h"
 #include "GPIO.h"
 #include "GPT.h"
+#include "stk_interface.h"
 #include "Mcu_HW.h"
 #include "common_macros.h"
 
@@ -10,25 +11,33 @@
 #define REQUIRED_TIME_MS 200 
 #define REQUIRED_TICKS  REQUIRED_TIME_MS*ACTIVE_FREQ_MHZ*1000
 
+
+	void callbackfun(void){
+		
+			if(Dio_ReadChannel(Channel_A0)==0){
+			Dio_WriteChannel(Channel_A0, 1);
+		}
+		else {
+			Dio_WriteChannel(Channel_A0, 0);
+		}	
+	}
+	
 int main(){
 	
 	SET_BIT(RCC_RCGCGPIO, RCC_GPIOA_EN);
   SET_BIT(RCC_RCGCTIMER, RCC_TIMER0_EN);
 	
 	Port_Init(&Port_Cfg);
-	Gpt_Init(&Gpt_Cfg);
+	//Gpt_Init(&Gpt_Cfg);
+	//Gpt_StartTimer(TIMER0_16_32, REQUIRED_TICKS);
 	
-	Gpt_StartTimer(TIMER0_16_32, REQUIRED_TICKS);
+	MSTK_voidInit(STK_CLK_SRC_AHB);
+	
+	MSTK_voidSetIntervalPeriodic(1000, SYSTICK_TICKS_MS , &callbackfun);
+	
 	
 	while(1){
-		while(BIT_IS_CLEAR(GPTMRIS(TIMER0_16_32), 0));
-		if(Dio_ReadChannel(Channel_A0)==0){
-			Dio_WriteChannel(Channel_A0, 1);
-		}
-		else {
-			Dio_WriteChannel(Channel_A0, 0);
-		}
-		SET_BIT(GPTMICR(TIMER0_16_32), 0);
+
 	}
 	
 	return 0;
